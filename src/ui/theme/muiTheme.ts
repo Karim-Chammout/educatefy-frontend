62d581d1510ebab8522d22682a41a012a10bc77b
Karim <chammout.karim@gmail.com>
2024-12-14 01:05:45 +0200

    Create `muiTheme` ojbect

---
+++ src/ui/theme/muiTheme.ts
@@ -0,0 +1,31 @@
+import { ThemeOptions } from '@mui/material';
+
+import theme from './theme';
+
+const muiCustomTheme: ThemeOptions = {
+  palette: {
+    primary: {
+      main: theme.colors.blue,
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 360,
+      sm: 768,
+      md: 990,
+      lg: 1200,
+      xl: 1400,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+      },
+    },
+  },
+};
+
+export default muiCustomTheme;

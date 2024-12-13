a676320b7267ada577b9b60cb00d50ddc4951567
Karim <chammout.karim@gmail.com>
2024-12-14 00:06:18 +0200

    Wrap the `ErrorBoundary` around the `App`

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './i18n';
+import ErrorBoundary from '@/layout/ErrorBoundary';
+
 import App from './App';
+import './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 );

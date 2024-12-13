b666c452a2a35ba52d28dd9721bd588b69ffffbf
Karim <chammout.karim@gmail.com>
2024-12-14 01:46:50 +0200

    Create `logout` util

---
+++ src/utils/logout.ts
@@ -0,0 +1,14 @@
+import { client, isLoggedIn } from '@/layout/apolloClient';
+
+export const terminatSession = async () => {
+  localStorage.removeItem('JWT');
+  localStorage.removeItem('refreshToken');
+  isLoggedIn(false);
+  await client.clearStore();
+};
+
+export const logout = async () => {
+  await terminatSession();
+
+  return (window.location.href = '/login');
+};

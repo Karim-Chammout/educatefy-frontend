034d1bf28d9610b59e0273a2ff36afbf8e146f12
Karim <chammout.karim@gmail.com>
2024-12-14 01:09:29 +0200

    Add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider as MUIThemeProvider, createTheme } from '@mui/material/styles';
+
+import muiCustomTheme from '@/ui/theme/muiTheme';
+import theme from '@/ui/theme/theme';
+
 const App = () => {
+  const muiTheme = createTheme(muiCustomTheme);
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <MUIThemeProvider theme={muiTheme}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <h1>Hello World!</h1>
+      </ThemeProvider>
+    </MUIThemeProvider>
   );
 };
 

7df180613901a383355a85e6e95dc5f43b3dc3b0
Karim <chammout.karim@gmail.com>
2024-12-14 17:09:59 +0200

    Create `OIDCButtons`

---
+++ src/ui/compositions/AuthView/OIDCButtons.tsx
@@ -0,0 +1,55 @@
+import { useQuery } from '@apollo/client';
+import GoogleIcon from '@mui/icons-material/Google';
+import { ReactNode } from 'react';
+
+import { OpenIdClientDocument, OpenIdClientQuery } from '@/generated/graphql';
+import { BASE_URL } from '@/layout/apolloClient';
+import { Button, Loader } from '@/ui/components';
+
+type OIDCButtonsType = {
+  isRegister?: boolean;
+  userType?: 'student' | 'teacher';
+  disabled?: boolean;
+};
+
+const redirectURL = `${BASE_URL}/api/openid/redirect/`;
+
+const buttonIcon: { [key: string]: ReactNode } = {
+  google: <GoogleIcon />,
+};
+
+const OIDCButtons = ({ isRegister, userType, disabled }: OIDCButtonsType) => {
+  const { error, data, loading } = useQuery<OpenIdClientQuery>(OpenIdClientDocument);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error || !data) {
+    // TODO: add error page
+    return 'something went wrong';
+  }
+
+  return data.openIdClients.map((oidc) => {
+    const url = isRegister
+      ? `${redirectURL}${oidc.id}?userRole=${userType}`
+      : `${redirectURL}${oidc.id}`;
+
+    return (
+      <Button
+        key={oidc.id}
+        fullWidth
+        variant="contained"
+        sx={{ my: 3 }}
+        LinkComponent="a"
+        href={url}
+        startIcon={buttonIcon[oidc.identity_provider]}
+        disabled={disabled}
+      >
+        {oidc.button_text}
+      </Button>
+    );
+  });
+};
+
+export default OIDCButtons;

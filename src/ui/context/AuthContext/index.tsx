c50eb654e463f81fd47192c596874e59747129fc
Karim <chammout.karim@gmail.com>
2024-12-14 15:16:58 +0200

    Create `AuthContext`

---
+++ src/ui/context/AuthContext/index.tsx
@@ -0,0 +1,58 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import { useLocation } from 'react-router';
+
+type AuthContextType = {
+  authModal: {
+    isAuthModalVisible: boolean;
+    setModalVisibility: Dispatch<SetStateAction<boolean>>;
+    setAuthModalVisibility: (kind: 'login' | 'register') => void;
+    authModalType: 'login' | 'register' | undefined;
+  };
+};
+
+export const AuthContext = createContext({} as AuthContextType);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [authModalType, setAuthModalType] = useState<'login' | 'register' | undefined>(undefined);
+  const [isAuthModalVisible, setModalVisibility] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setModalVisibility(false);
+  }, [location, setModalVisibility]);
+
+  const setAuthModalVisibility = useCallback(
+    (kind: 'login' | 'register') => {
+      if (!isAuthModalVisible || kind === undefined) {
+        setModalVisibility((prev) => !prev);
+      }
+
+      setAuthModalType(kind);
+    },
+    [isAuthModalVisible],
+  );
+
+  const value = useMemo(
+    () => ({
+      authModal: {
+        isAuthModalVisible,
+        setModalVisibility,
+        setAuthModalVisibility,
+        authModalType,
+      },
+    }),
+    [isAuthModalVisible, setAuthModalVisibility, authModalType],
+  );
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}

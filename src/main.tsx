7435d975bc81a20f673ade0260df20b2f1537d2e
Karim <chammout.karim@gmail.com>
2024-12-14 16:42:08 +0200

    Wrap the `ToasterProvider` around the `App`

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import ErrorBoundary from '@/layout/ErrorBoundary';
 import { Loader } from '@/ui/components';
-import { AuthProvider } from '@/ui/context';
+import { AuthProvider, ToasterProvider } from '@/ui/context';
 
 import App from './App';
 import './i18n';
@@ -12,9 +12,11 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Suspense fallback={<Loader />}>
       <ErrorBoundary>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <ToasterProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </ToasterProvider>
       </ErrorBoundary>
     </Suspense>
   </StrictMode>,

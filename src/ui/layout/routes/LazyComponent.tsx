389a59cab1603f3e2f6c039a8a5ab5ad4ae12d8d
Karim <chammout.karim@gmail.com>
2024-12-15 01:34:50 +0200

    Create `LazyComponent` file

---
+++ src/ui/layout/routes/LazyComponent.tsx
@@ -0,0 +1,8 @@
+import { lazy } from 'react';
+
+export const Explore = lazy(() => import('@/ui/pages/Explore'));
+export const Home = lazy(() => import('@/ui/pages/Home'));
+export const Login = lazy(() => import('@/ui/pages/Login'));
+export const LoginCallback = lazy(() => import('@/ui/pages/Login/LoginCallback'));
+export const NotFound = lazy(() => import('@/ui/pages/NotFound'));
+export const Register = lazy(() => import('@/ui/pages/Register'));

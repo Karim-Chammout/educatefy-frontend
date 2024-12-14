3edb98fd00780c313b082f3452c74529aec9f6fc
Karim <chammout.karim@gmail.com>
2024-12-14 14:53:22 +0200

    Use `Loader` component as a fallback for `Suspense`

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import ErrorBoundary from '@/layout/ErrorBoundary';
+import { Loader } from '@/ui/components';
 
 import App from './App';
 import './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Suspense fallback={<p>Loading...</p>}>
+    <Suspense fallback={<Loader />}>
       <ErrorBoundary>
         <App />
       </ErrorBoundary>

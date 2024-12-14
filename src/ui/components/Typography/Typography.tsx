112d562831e0e5b3711f9a335322e125b9273363
Karim <chammout.karim@gmail.com>
2024-12-14 14:49:07 +0200

    Create `Typography` component

---
+++ src/ui/components/Typography/Typography.tsx
@@ -0,0 +1,47 @@
+import { SxProps, Theme, TypeText } from '@mui/material';
+import MuiTypography from '@mui/material/Typography';
+import { Variant } from '@mui/material/styles/createTypography';
+import { ElementType, ReactNode } from 'react';
+
+const Typography = ({
+  children,
+  variant,
+  gutterBottom,
+  component,
+  sx,
+  color,
+  dangerouslySetInnerHTML,
+}: {
+  children?: ReactNode;
+  variant?: Variant;
+  component?: ElementType;
+  gutterBottom?: boolean;
+  sx?: SxProps<Theme>;
+  color?:
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'error'
+    | 'info'
+    | 'warning'
+    | `text${Capitalize<keyof TypeText>}`
+    | string;
+  dangerouslySetInnerHTML?: {
+    __html: string;
+  };
+}) => {
+  return (
+    <MuiTypography
+      variant={variant}
+      component={component || 'p'}
+      gutterBottom={gutterBottom}
+      color={color}
+      sx={{ ...sx }}
+      dangerouslySetInnerHTML={dangerouslySetInnerHTML}
+    >
+      {children}
+    </MuiTypography>
+  );
+};
+
+export default Typography;

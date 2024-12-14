86d83f1ddc0ab40a08e425556dd774c8891c12c7
Karim <chammout.karim@gmail.com>
2024-12-14 15:23:13 +0200

    Generate GQL types

---
+++ src/generated/graphql.ts
@@ -0,0 +1,283 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  Date: { input: any; output: any; }
+};
+
+/** The properties of the account */
+export type Account = {
+  __typename?: 'Account';
+  /** The role of the account. */
+  accountRole: AccountRole;
+  /** The avatar url of this account (provided by the openid-provider) */
+  avatar_url?: Maybe<Scalars['String']['output']>;
+  /** The bio of the teacher. */
+  bio?: Maybe<Scalars['String']['output']>;
+  /** The current country the user stated to live in. */
+  country?: Maybe<Country>;
+  /** The date of birth of this account */
+  date_of_birth?: Maybe<Scalars['Date']['output']>;
+  /** A detailed overview about this teacher. */
+  description?: Maybe<Scalars['String']['output']>;
+  /** The first name of the account */
+  first_name?: Maybe<Scalars['String']['output']>;
+  /** The gender of the account */
+  gender?: Maybe<Gender>;
+  /** A unique id of this account */
+  id: Scalars['ID']['output'];
+  /** The last name of the account */
+  last_name?: Maybe<Scalars['String']['output']>;
+  /** The name of the account */
+  name?: Maybe<Scalars['String']['output']>;
+  /** The nationality of the user */
+  nationality?: Maybe<Country>;
+  /** The nickname of the account */
+  nickname?: Maybe<Scalars['String']['output']>;
+  /** Represents the subject a teacher is specialized in for teaching. */
+  specialty?: Maybe<Scalars['String']['output']>;
+};
+
+/** Input for updating an account information */
+export type AccountInfoInput = {
+  /** The current country of the user */
+  countryId: Scalars['ID']['input'];
+  /** The date of birth of the user. */
+  dateOfBirth: Scalars['Date']['input'];
+  /** The first name of the user. */
+  firstName: Scalars['String']['input'];
+  /** The gender of the user. */
+  gender: Gender;
+  /** The last name of the user. */
+  lastName: Scalars['String']['input'];
+  /** The nationality of the user */
+  nationalityId: Scalars['ID']['input'];
+  /** The nickname name of the user. */
+  nickname: Scalars['String']['input'];
+  /** The short bio about the teacher. */
+  teacherBio?: InputMaybe<Scalars['String']['input']>;
+  /** The short description about the teacher. */
+  teacherDescription?: InputMaybe<Scalars['String']['input']>;
+  /** The specialty of the teacher. */
+  teacherSpecialty?: InputMaybe<Scalars['String']['input']>;
+};
+
+/** The role of the account. */
+export enum AccountRole {
+  Student = 'student',
+  Teacher = 'teacher'
+}
+
+/** The result of the changeProfilePicture mutation. */
+export type ChangeProfilePictureResult = {
+  __typename?: 'ChangeProfilePictureResult';
+  /** A list of errors that occurred executing this mutation. */
+  errors: Array<Error>;
+  /** Indicates if the mutation was successful. */
+  success: Scalars['Boolean']['output'];
+  /** The updated user information. */
+  user?: Maybe<Account>;
+};
+
+/** The country info */
+export type Country = {
+  __typename?: 'Country';
+  /** The name of this country */
+  denomination: Scalars['String']['output'];
+  /** A unique id of this country */
+  id: Scalars['ID']['output'];
+  /** The iso code of this country */
+  iso: Scalars['String']['output'];
+  /** The iso3 code of this country */
+  iso3?: Maybe<Scalars['String']['output']>;
+  /** The num_code code of this country */
+  num_code?: Maybe<Scalars['String']['output']>;
+  /** The phone_code code of this country */
+  phone_code: Scalars['String']['output'];
+};
+
+/** An object type that wraps an error */
+export type Error = {
+  __typename?: 'Error';
+  /** The message of this error. */
+  message: Scalars['String']['output'];
+};
+
+/** Gender of the account. */
+export enum Gender {
+  Female = 'female',
+  Male = 'male',
+  Other = 'other'
+}
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  /** Change the profile picture of a user. */
+  changeProfilePicture?: Maybe<ChangeProfilePictureResult>;
+  /** Remove the profile picture of a user. */
+  removeProfilePicture?: Maybe<ChangeProfilePictureResult>;
+  /** Updates a user account information. */
+  updateAccountInfo?: Maybe<MutationResult>;
+  /** Updates a user profile details. */
+  updateProfile?: Maybe<UpdateProfileResult>;
+};
+
+
+export type MutationChangeProfilePictureArgs = {
+  profilePictureDetails: ProfilePictureDetailsInput;
+};
+
+
+export type MutationUpdateAccountInfoArgs = {
+  accountInfo: AccountInfoInput;
+};
+
+
+export type MutationUpdateProfileArgs = {
+  profileDetails: ProfileDetailsInput;
+};
+
+/** The result of a mutation. */
+export type MutationResult = {
+  __typename?: 'MutationResult';
+  /** A list of errors that occurred executing this mutation. */
+  errors: Array<Error>;
+  /** Indicates if the mutation was successful. */
+  success: Scalars['Boolean']['output'];
+};
+
+/** The open id client providers to use for auth */
+export type OpenidClient = {
+  __typename?: 'OpenidClient';
+  /** The button background color of the OIDC */
+  button_background_color?: Maybe<Scalars['String']['output']>;
+  /** The icon of the OIDC that should be displayed on the button */
+  button_icon?: Maybe<Scalars['String']['output']>;
+  /** The text of the OIDC that should be displayed on the button */
+  button_text?: Maybe<Scalars['String']['output']>;
+  /** A unique id of this OIDC */
+  id: Scalars['ID']['output'];
+  /** The openId provider */
+  identity_provider: Scalars['String']['output'];
+};
+
+/** Input for updating the user profile details */
+export type ProfileDetailsInput = {
+  /** The current country of the user */
+  countryId?: InputMaybe<Scalars['ID']['input']>;
+  /** The date of birth of the user. */
+  dateOfBirth?: InputMaybe<Scalars['Date']['input']>;
+  /** The first name of the user. */
+  firstName?: InputMaybe<Scalars['String']['input']>;
+  /** The gender of the user. */
+  gender?: InputMaybe<Gender>;
+  /** The last name of the user. */
+  lastName?: InputMaybe<Scalars['String']['input']>;
+  /** The nationality of the user */
+  nationalityId?: InputMaybe<Scalars['ID']['input']>;
+  /** The nickname name of the user. */
+  nickname?: InputMaybe<Scalars['String']['input']>;
+  /** The short bio about the teacher. */
+  teacherBio?: InputMaybe<Scalars['String']['input']>;
+  /** The short description about the teacher. */
+  teacherDescription?: InputMaybe<Scalars['String']['input']>;
+  /** The specialty of the teacher. */
+  teacherSpecialty?: InputMaybe<Scalars['String']['input']>;
+};
+
+/** Input for updating an account information */
+export type ProfilePictureDetailsInput = {
+  /** The file path in the bucket. */
+  filePath: Scalars['String']['input'];
+  /** The file size in bytes of this file. */
+  fileSize: Scalars['Int']['input'];
+  /** The mime type of the file. */
+  mimeType: Scalars['String']['input'];
+  /** The original file name. */
+  originalFileName: Scalars['String']['input'];
+  /** The uuid of the file. */
+  uuid: Scalars['String']['input'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  /** List of countries */
+  countries: Array<Country>;
+  /** The current user */
+  me: Account;
+  /** List of OpenId clients */
+  openIdClients: Array<OpenidClient>;
+};
+
+/** The result of the updateProfile mutation. */
+export type UpdateProfileResult = {
+  __typename?: 'UpdateProfileResult';
+  /** A list of errors that occurred executing this mutation. */
+  errors: Array<Error>;
+  /** Indicates if the mutation was successful. */
+  success: Scalars['Boolean']['output'];
+  /** The updated user information. */
+  user?: Maybe<Account>;
+};
+
+export type AccountInfoQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type AccountInfoQuery = { __typename?: 'Query', me: { __typename?: 'Account', nickname?: string | null, gender?: Gender | null, avatar_url?: string | null, accountRole: AccountRole } };
+
+
+export const AccountInfoDocument = gql`
+    query AccountInfo {
+  me {
+    nickname
+    gender
+    avatar_url
+    accountRole
+  }
+}
+    `;
+
+/**
+ * __useAccountInfoQuery__
+ *
+ * To run a query within a React component, call `useAccountInfoQuery` and pass it any options that fit your needs.
+ * When your component renders, `useAccountInfoQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useAccountInfoQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useAccountInfoQuery(baseOptions?: Apollo.QueryHookOptions<AccountInfoQuery, AccountInfoQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<AccountInfoQuery, AccountInfoQueryVariables>(AccountInfoDocument, options);
+      }
+export function useAccountInfoLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<AccountInfoQuery, AccountInfoQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<AccountInfoQuery, AccountInfoQueryVariables>(AccountInfoDocument, options);
+        }
+export function useAccountInfoSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<AccountInfoQuery, AccountInfoQueryVariables>) {
+          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<AccountInfoQuery, AccountInfoQueryVariables>(AccountInfoDocument, options);
+        }
+export type AccountInfoQueryHookResult = ReturnType<typeof useAccountInfoQuery>;
+export type AccountInfoLazyQueryHookResult = ReturnType<typeof useAccountInfoLazyQuery>;
+export type AccountInfoSuspenseQueryHookResult = ReturnType<typeof useAccountInfoSuspenseQuery>;
+export type AccountInfoQueryResult = Apollo.QueryResult<AccountInfoQuery, AccountInfoQueryVariables>;

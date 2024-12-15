33460e0e385ce7ad179a389daff9911a345fb4b9
Karim <chammout.karim@gmail.com>
2024-12-15 12:13:42 +0200

    Create `genderOptions` util

---
+++ src/utils/genderOptions.ts
@@ -0,0 +1,16 @@
+import { Gender } from '@/generated/graphql';
+
+export const genderOptions = [
+  {
+    id: Gender.Male,
+    label: 'Male',
+  },
+  {
+    id: Gender.Female,
+    label: 'Female',
+  },
+  {
+    id: Gender.Unknown,
+    label: 'Prefer not to say',
+  },
+];

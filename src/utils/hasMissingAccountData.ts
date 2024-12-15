5a0fb33d4a4ef4e87632299579eaab397f23fa4e
Karim <chammout.karim@gmail.com>
2024-12-15 12:13:53 +0200

    Create `hasMissingAccountData` util

---
+++ src/utils/hasMissingAccountData.ts
@@ -0,0 +1,18 @@
+import { AccountFragment, AccountRole } from '@/generated/graphql';
+
+export const hasMissingAccountData = (account: AccountFragment | null | undefined) => {
+  if (
+    account &&
+    (!account.date_of_birth ||
+      !account.gender ||
+      !account.nickname ||
+      !account.nationality ||
+      !account.country ||
+      (account.accountRole === AccountRole.Teacher &&
+        (!account.specialty || !account.bio || !account.description)))
+  ) {
+    return true;
+  }
+
+  return false;
+};

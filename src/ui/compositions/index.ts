22ed505dd17a3306df0862098fbc6c14028e3bc3
Karim <chammout.karim@gmail.com>
2024-12-16 17:38:29 +0200

    Export `RichTextEditor`

--- src/ui/compositions/index.ts
+++ src/ui/compositions/index.ts
@@ -3,4 +3,5 @@ export { default as ErrorPlaceholder } from './ErrorPlaceholder';
 export { default as FileDropzone } from './FileDropzone';
 export { default as InfoState } from './InfoState';
 export { default as LanguageSelector } from './LanguageSelector';
+export { default as RichTextEditor } from './RichTextEditor';
 export { default as SetupProfile } from './SetupProfile';

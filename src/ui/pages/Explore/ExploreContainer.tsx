8d869e402b20372149b65b31d5ec07ef888db241
Karim <chammout.karim@gmail.com>
2024-12-23 00:41:30 +0100

    Display toaster for permission denied access in Explore page

--- src/ui/pages/Explore/ExploreContainer.tsx
+++ src/ui/pages/Explore/ExploreContainer.tsx
@@ -1,6 +1,28 @@
+import { useContext, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router';
+
+import { ToasterContext } from '@/ui/context';
+import { PERMISSION_DENIED } from '@/utils/constants';
+
 import Explore from './Explore';
 
 const ExploreContainer = () => {
+  const location = useLocation();
+  const { setToasterVisibility } = useContext(ToasterContext);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    // Display a toaster when a user tries to access a page without permissions
+    if (location.state?.action === PERMISSION_DENIED) {
+      setToasterVisibility({
+        newDuration: 5000,
+        newType: 'error',
+        newText: t('explore.permissionDenied'),
+      });
+    }
+  }, [location.state?.action]);
+
   return (
     <div>
       <Explore />

4cfbfcd0b4ef9dae518bb60e9f650eab1dd3b954
Karim <chammout.karim@gmail.com>
2024-12-31 15:23:38 +0100

    Fix `languageDirection` prop forwarding

--- src/ui/compositions/RichTextEditor/RichTextEditor.style.ts
+++ src/ui/compositions/RichTextEditor/RichTextEditor.style.ts
@@ -2,32 +2,32 @@ import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 
-export const EditorWrapper = styled(Box)<{ languageDirection: string }>(
-  ({ theme, languageDirection }) => ({
-    position: 'relative',
-    width: '100%',
+export const EditorWrapper = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'languageDirection',
+})<{ languageDirection: string }>(({ theme, languageDirection }) => ({
+  position: 'relative',
+  width: '100%',
 
-    '& .ql-toolbar': {
-      direction: languageDirection,
-      borderColor: theme.palette.divider,
-      borderRadius: `${theme.shape.borderRadius}px ${theme.shape.borderRadius}px 0 0`,
+  '& .ql-toolbar': {
+    direction: languageDirection,
+    borderColor: theme.palette.divider,
+    borderRadius: `${theme.shape.borderRadius}px ${theme.shape.borderRadius}px 0 0`,
+  },
+  '& .ql-container': {
+    borderColor: theme.palette.divider,
+    borderRadius: `0 0 ${theme.shape.borderRadius}px ${theme.shape.borderRadius}px`,
+    fontFamily: theme.typography.fontFamily,
+  },
+  '& .ql-editor': {
+    minHeight: '200px',
+    fontFamily: theme.typography.fontFamily,
+    fontSize: theme.typography.body1.fontSize,
+    '&.ql-blank::before': {
+      fontStyle: 'normal',
+      color: theme.palette.text.secondary,
     },
-    '& .ql-container': {
-      borderColor: theme.palette.divider,
-      borderRadius: `0 0 ${theme.shape.borderRadius}px ${theme.shape.borderRadius}px`,
-      fontFamily: theme.typography.fontFamily,
-    },
-    '& .ql-editor': {
-      minHeight: '200px',
-      fontFamily: theme.typography.fontFamily,
-      fontSize: theme.typography.body1.fontSize,
-      '&.ql-blank::before': {
-        fontStyle: 'normal',
-        color: theme.palette.text.secondary,
-      },
-    },
-  }),
-);
+  },
+}));
 
 export const LoadingOverlay = styled(Backdrop)(({ theme }) => ({
   position: 'absolute',

f5338fb5378b83385a271400f22bb6082d3ea812
Karim <chammout.karim@gmail.com>
2024-12-31 13:00:27 +0100

    Add `InfoState` for empty courses state

--- src/ui/pages/Dashboard/Courses/CoursesContainer.tsx
+++ src/ui/pages/Dashboard/Courses/CoursesContainer.tsx
@@ -1,10 +1,17 @@
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useNavigate } from 'react-router';
+import { useTranslation } from 'react-i18next';
+
 import { useTeacherCoursesQuery } from '@/generated/graphql';
 import { Loader } from '@/ui/components';
-import { ErrorPlaceholder } from '@/ui/compositions';
+import { ErrorPlaceholder, InfoState } from '@/ui/compositions';
 
 import Courses from './Courses';
 
 const CoursesContainer = () => {
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+
   const { loading, error, data } = useTeacherCoursesQuery();
 
   if (loading) {
@@ -13,6 +20,18 @@ const CoursesContainer = () => {
 
   if (error || !data) {
     return <ErrorPlaceholder />;
+  }
+
+  if (data.teacherCourses.length === 0) {
+    return (
+      <InfoState
+        btnLabel={t('courses.createNewCourse')}
+        btnOnClick={() => navigate('/dashboard/courses/create')}
+        subtitle={t('courses.noCoursesSubtitle')}
+        title={t('courses.noCoursesTitle')}
+        icon={<AddCircleOutlineIcon />}
+      />
+    );
   }
 
   return <Courses courses={data.teacherCourses} />;

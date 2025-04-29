c78e7cf522c0e19dfb2a09d13c1434cb716dfb6f
Karim <chammout.karim@gmail.com>
2025-04-29 23:33:56 +0200

    Add index file to export composition

---
+++ src/ui/pages/Dashboard/Courses/UpdateCourse/CourseSectionItem/composition/index.ts
@@ -0,0 +1,3 @@
+export { default as ComponentCreationForm } from './ComponentCreationForm';
+export { default as DraggableComponentItem } from './DraggableComponentItem';
+export { default as useComponentManagement } from './useComponentManagement';

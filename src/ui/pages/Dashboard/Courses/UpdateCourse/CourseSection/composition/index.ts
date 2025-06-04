5373989a29a6347df73982fe1b9084621b3f3e7a
Karim <chammout.karim@gmail.com>
2025-06-05 00:46:07 +0200

    Export `ItemEditForm` from composition index

--- src/ui/pages/Dashboard/Courses/UpdateCourse/CourseSection/composition/index.ts
+++ src/ui/pages/Dashboard/Courses/UpdateCourse/CourseSection/composition/index.ts
@@ -1,2 +1,3 @@
 export { default as DraggableItem } from './DraggableItem';
 export { default as ItemCreationForm } from './ItemCreationForm';
+export { default as ItemEditForm } from './ItemEditForm';

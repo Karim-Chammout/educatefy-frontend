e390dc9e9a0ef0d5f02048c14a81335c0dfcc34c
Karim <chammout.karim@gmail.com>
2025-06-21 19:11:47 +0200

    Add TypeScript declarations for MUI BreakpointOverrides

---
+++ src/types/mui.d.ts
@@ -0,0 +1,12 @@
+import '@mui/material';
+
+declare module '@mui/material/styles' {
+  interface BreakpointOverrides {
+    xxs: true;
+    xs: true;
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
+  }
+}

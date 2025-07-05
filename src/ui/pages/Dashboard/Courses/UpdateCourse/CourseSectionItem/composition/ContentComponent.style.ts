df51232ec2849c3dd08df6fbb7c61c9d3a6cab75
Karim <chammout.karim@gmail.com>
2025-07-05 19:27:31 +0200

    Create `ContentComponent` style file

---
+++ src/ui/pages/Dashboard/Courses/UpdateCourse/CourseSectionItem/composition/ContentComponent.style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+export const CheckBoxWrapper = styled('div')(css`
+  display: flex;
+  align-items: center;
+  gap: 1;
+  margin: 16px 0;
+`);

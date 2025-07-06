2529216a37830a9da026f0b193fe315e81691e81
Karim <chammout.karim@gmail.com>
2025-07-06 21:31:59 +0200

    Add composition exports index file

---
+++ src/ui/pages/Course/Section/composition/index.ts
@@ -0,0 +1,8 @@
+export { default as ActionButtons } from './ActionButtons';
+export { default as ContentRenderer } from './ContentRenderer';
+export { default as ComponentHeader } from './ComponentHeader';
+export { default as LockedContent } from './LockedContent';
+export { default as NavigationItem } from './NavigationItem';
+export { default as SectionHeader } from './SectionHeader';
+export { default as SectionNavigation } from './SectionNavigation';
+export { default as SectionLoader } from './SectionLoader';
